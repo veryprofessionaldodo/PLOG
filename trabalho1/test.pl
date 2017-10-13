5384c5661a54d2e67f02d9ff2b500eb03a18693c
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-10-13 12:59:12 +0100

    Board now displaying recursively

--- trabalho1/test.pl
+++ trabalho1/test.pl
@@ -1,11 +1,38 @@
 /* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:3; tab-width:8; -*- */
 
-print_board :- print_line, print_line, print_line,print_line, print_line, print_line,print_line, print_line, print_top.
-print_line :- print_top , print_edge, print_character, print_edge,print_character, print_edge,print_character, print_edge,print_character, print_edge,print_character, print_edge,print_character, print_edge,print_character, print_edge,print_character, print_edge,print_character, print_edge,print_character, print_edge,  print_newline.
-print_top :- write('-------------------------------------------------------------\n').
+board( [['w','w','w','w','w','w','w','w','w','w'],
+	   [' ',' ',' ',' ','W',' ',' ',' ',' ',' '],
+	   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+	   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+	   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+	   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+	   [' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
+	   [' ',' ',' ',' ',' ','B',' ',' ',' ',' '],
+	   ['b','b','b','b','b','b','b','b','b','b']]).
 
-print_edge:- write('|').
+/* If line reached the end, stop, and print a new line. */
+print_line([]) :- write('|'), print_newline.
 
+/* If line has content, continue reading until the end. */
+print_line( [Head|Tail]) :- print_character(Head), print_line(Tail).
+ 
+/* If board has reached the end, print the final separating line. */
+print_board([]) : print_top.
+
+/* While board has content, continue to print lines. */
+print_board([Head|Tail]) :- print_top, print_line(Head), print_board(Tail).
+
+print_board :- board(X), print_board(X).
+
+
+/* Print specific character. */
+print_character(X) :- write('|  '), write(X), write('  ').
+
+/* Print separating line. */
+print_top :- write('---------------------------------------------------------------\n').
+
+/* Print on a new line. */
 print_newline :- write('\n').
 
-print_character :- write('  '), write('W'), write('  ').
+getPeca(Tabuleiro,Coluna,Linha,Peca):- nth1(Coluna, Tabuleiro, X), nth1(Linha, X, Peca).
+
