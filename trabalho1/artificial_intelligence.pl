a79cc9d9faf60ce4d946b25a36c7db863abe2c4e
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-11-08 10:47:55 +0000

    Nearly done gathering all moves

--- trabalho1/artificial_intelligence.pl
+++ trabalho1/artificial_intelligence.pl
@@ -12,33 +12,52 @@
 % 1 - Position to enemy pawn
 % 0 - No available move that attack enemy, random move selected.
 
-AILevel(1).
-AILevel(2).
-
 % Gather all moves for all pieces.
-gather_all_moves(AIDifficulty, ListOfMoves) :- AILevel(AIDifficulty), write('\nThinking...\n'), board(Board), gather_moves_recursive(Board, ListOfMoves, 0), write(ListOfMoves).
+gather_all_moves([ListOfMoves|FinalList], Player) :- write('\nThinking...\n'), board(Board), gather_moves_recursive(Board, ListOfMoves, 8, Player), write(ListOfMoves), FinalList is ListOfMoves.
 
 % End of recursion.
-gather_moves_recursive([], ListOfMoves, RowNumber).
+gather_moves_recursive([], ListOfMoves, RowNumber, Player).
 
 % Recursive function that calls itself to see all the pieces.
-gather_moves_recursive([Row|Tail], ListOfMoves, RowNumber) :- gather_moves_by_row(Row, ListOfMoves, RowNumber, 0), NewRow is RowNumber + 1, gather_moves_recursive(Tail, ListOfMoves, NewRow).
+gather_moves_recursive([Row|Tail], ListOfMoves, RowNumber, Player) :- gather_moves_by_row(Row, ListOfMoves, RowNumber, 0, Player), NewRow is RowNumber - 1, gather_moves_recursive(Tail, ListOfMoves, NewRow).
 
 % Has reached the ending row.
-gather_moves_by_row([], ListOfMoves, RowNumber, ColumnNumber).
+gather_moves_by_row([], ListOfMoves, RowNumber, ColumnNumber, Player).
 
 % Gather all pieces in a Row.
-gather_moves_by_row([Piece|Tail], ListOfMoves, RowNumber, ColumnNumber) :- gather_moves_piece(Piece, RowNumber, ColumnNumber, ListOfMoves), NewColumn is ColumnNumber + 1, 
+gather_moves_by_row([Piece|Tail], ListOfMoves, RowNumber, ColumnNumber, Player) :- (player_letter(Player, Piece), write('\nPeça.'),gather_moves_piece(Piece, RowNumber, ColumnNumber, ListOfMoves)) ; NewColumn is ColumnNumber + 1, 
 			gather_moves_by_row(Tail, ListOfMoves, RowNumber, NewColumn). 
-/*
+
 % Gather all moves in a specific place.			
 gather_moves_piece(Piece, RowNumber, ColumnNumber, ListOfMoves) :-
-		NewColumn1 is ColumnNumber - 1, gather_moves_left(ListOfMoves, ColumnNumber, RowNumber, NewColumn1, RowNumber),
-		NewColumn2 is ColumnNumber + 1, gather_moves_rigth(ListOfMoves, ColumnNumber, RowNumber, NewColumn2, RowNumber),
-		NewRow1 is RowNumber - 1, gather_moves_down(ListOfMoves, ColumnNumber, RowNumber, ColumnNumber, NewRow1),
-		NewRow2 is RowNumber + 1, gather_moves_up(ListOfMoves, ColumnNumber, RowNumber, ColumnNumber, NewRow2).
+		write('Left\n'),NewColumn1 is ColumnNumber - 1, gather_moves_left(ListOfMoves, ColumnNumber, RowNumber, NewColumn1, RowNumber), write(ListOfMoves),
+		write('Right\n'),NewColumn2 is ColumnNumber + 1, gather_moves_right(ListOfMoves, ColumnNumber, RowNumber, NewColumn2, RowNumber), write(ListOfMoves),
+		write('Down\n'),NewRow1 is RowNumber - 1, gather_moves_down(ListOfMoves, ColumnNumber, RowNumber, ColumnNumber, NewRow1), write(ListOfMoves),
+		write('Up\n'),NewRow2 is RowNumber + 1, gather_moves_up(ListOfMoves, ColumnNumber, RowNumber, ColumnNumber, NewRow2), write(ListOfMoves).
 
-gather_moves_left(ListOfMoves, OriginalRow, OriginalColumn, ColumnNumber, RowNumber) :-  NewColumn1 is ColumnNumber - 1, NewColumn1 > 0, get_piece(NewColumn1, RowNumber, Piece), Piece = ' '. */
+gather_moves_left(ListOfMoves, OriginalColumn, OriginalRow, ColumnNumber, RowNumber) :-  NewColumn1 is ColumnNumber - 1, NewColumn1 > 0, get_piece(NewColumn1, RowNumber, Piece), Piece = ' ',
+		NewMove is [OriginalColumn, OriginalRow, NewColumn1, RowNumber], NewList is [NewMove|ListOfMoves], gather_moves_left(NewList, OriginalColumn, OriginalRow, NewColumn1, RowNumber).
+
+% Has reached the end of column, or hit another piece.
+gather_moves_left(ListOfMoves, OriginalRow, OriginalColumn, ColumnNumber, RowNumber). 
+
+gather_moves_right(ListOfMoves, OriginalColumn, OriginalRow, ColumnNumber, RowNumber) :-NewColumn1 is ColumnNumber + 1, NewColumn1 < 11, get_piece(NewColumn1, RowNumber, Piece), Piece = ' ',
+		NewMove is [OriginalColumn, OriginalRow, NewColumn1, RowNumber], NewList is [NewMove|ListOfMoves], gather_moves_right(NewList, OriginalColumn, OriginalRow, NewColumn1, RowNumber).
+
+% Has reached the end of column, or hit another piece.
+gather_moves_right(ListOfMoves, OriginalRow, OriginalColumn, ColumnNumber, RowNumber). 
+
+gather_moves_up(ListOfMoves, OriginalColumn, OriginalRow, ColumnNumber, RowNumber) :-NewRow1 is RowNumber + 1, NewRow1 < 9, get_piece(ColumnNumber, NewRow1, Piece), Piece = ' ',
+		NewMove is [OriginalColumn, OriginalRow, ColumnNumber, NewRow1], NewList is [NewMove|ListOfMoves], gather_moves_up(NewList, OriginalColumn, OriginalRow, ColumnNumber, NewRow1).
+
+% Has reached the end of row, or hit another piece.
+gather_moves_up(ListOfMoves, OriginalRow, OriginalColumn, ColumnNumber, RowNumber). 
+
+gather_moves_down(ListOfMoves, OriginalColumn, OriginalRow, ColumnNumber, RowNumber) :- NewRow1 is RowNumber - 1, NewRow1 > 0, get_piece(ColumnNumber, NewRow1, Piece), Piece = ' ',
+		NewMove is [OriginalColumn, OriginalRow, ColumnNumber, NewRow1], NewList is [NewMove|ListOfMoves], gather_moves_down(NewList, OriginalColumn, OriginalRow, ColumnNumber, NewRow1).
+
+% Has reached the end of row, or hit another piece.
+gather_moves_down(ListOfMoves, OriginalRow, OriginalColumn, ColumnNumber, RowNumber). 
 
 /* ORDEM DE PRIORIDADES 
 	1a O Dux inimigo está exposto?
