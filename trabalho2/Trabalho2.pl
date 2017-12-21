986e5746a6bc1ba0b7d7143c5d9d0603ed19250a
rjstorres <r_torres96@live.com.pt>
2017-12-21 16:20:35 +0000

    now actually done!!!

--- trabalho2/Trabalho2.pl
+++ trabalho2/Trabalho2.pl
@@ -12,11 +12,11 @@ criteria_priority('Software', 0.3).
 criteria_priority('Hardware', 0.2).
 
 %measure(Id, Description, Cost, Bonus([Id-Improvement]), Setbacks([Id-Impact])).
-measure('Tutorials', 100, [0-2000, 1-4000], [2-1000]).
-measure('New Computers', 500, [2-5000, 1-8000], [0-1000]).
-measure('Hire more staff', 900, [0-10000, 1-20000], [2-2000, 3-3000]).
-measure('Improve food quality', 200, [0-4000, 1-2000], []).
-measure('In house software', 1000, [0-10000, 1-20000], [0-3000]).
+measure('Tutorials', 100, [0-2, 1-4], [2-1]).
+measure('New Computers', 500, [2-5, 1-8], [0-1]).
+measure('Hire more staff', 900, [0-10, 1-20], [2-2, 3-3]).
+measure('Improve food quality', 200, [0-4, 1-2], []).
+measure('In house software', 1000, [0-10, 1-20], [0-3]).
 
 
 getCostImpact(AllCost,AllImpact):- findall(Priority,(criteria_priority(_,Priority)), AllPriorities),
@@ -29,18 +29,18 @@ getTotalImpact([] ,[], Helper, Impact,_):- Impact = Helper.
 getTotalImpact([] ,[PN-IN|RN], Helper, Impact,AllPriorities):-  
 
         nth0(PN, AllPriorities,NImp),
-        Newhelper is Helper -NImp*IN,
+        Newhelper is Helper -NImp*IN*1000,
         getTotalImpact([],RN,Newhelper,Impact,AllPriorities).
         
 getTotalImpact([PP-IP|RP], [], Helper, Impact,AllPriorities):-
         nth0(PP, AllPriorities, Imp),     
-        Newhelper is Helper + (Imp *IP),
+        Newhelper is Helper + (Imp *IP*1000),
         getTotalImpact(RP,[],Newhelper,Impact,AllPriorities).           
         
 getTotalImpact([PP-IP|RP],[PN-IN|RN], Helper, Impact,AllPriorities):-  
         nth0(PP, AllPriorities, Imp),
         nth0(PN, AllPriorities,NImp),
-        Newhelper is Helper + (Imp *IP-NImp*IN),
+        Newhelper is Helper + (Imp *IP*1000-NImp*IN*1000),
         getTotalImpact(RP,RN,Newhelper,Impact,AllPriorities).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
