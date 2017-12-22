fd90d1e113c14e3a3d24687c12c4ba362a74ee4d
Pedro Daniel Reis <up201506046@fe.up.pt>
2017-12-22 18:18:34 +0000

    Improvements to performance, more measures & priorities

--- trabalho2/Trabalho2.pl
+++ trabalho2/Trabalho2.pl
@@ -1,81 +1,37 @@
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
 :-reconsult(utils).
+:-reconsult(test_cases).
 
-% criteria_priority(Id,Description, Priority).
-criteria_priority('Employee Efficiency', 0.4).
-criteria_priority('Public Relations', 0.1).
-criteria_priority('Software', 0.3).
-criteria_priority('Hardware', 0.2).
+% Function to be called from SICStus.
+solveProblem(Budget):-
+  statistics(walltime, [TimeSinceStart | [TimeSinceLastCall]]),
+  getCostImpact(AllCost,AllImpact), 
+  optimize(AllCost,AllImpact,Result,Budget),
+  showResult(Result),
+  statistics(walltime, [NewTimeSinceStart | [ExecutionTime]]),
+  write('Execution took '), write(ExecutionTime), write(' ms.'), nl.
+   
+% Measures the total impact of all measures.
+getCostImpact(AllCost, AllImpact) :-
+  findall(Priority,(criteria_priority(_,Priority)), AllPriorities),
+  sumlist(AllPriorities, TotalPriority), 
+  RoundedTotal is round(TotalPriority),
+  RoundedTotal == 1,
+  findall(ImpactCost, (measure(_Description, Cost, Positives, Negatives), once(measureImpact(AllPriorities, Cost, Positives, Negatives, ImpactCost))), AllImpact),
+  findall(Cost,measure(_, Cost,_,_), AllCost).
 
-%measure(Id, Description, Cost, Bonus([Id-Improvement]), Setbacks([Id-Impact])).
-measure('Tutorials', 100, [0-2, 1-4], [2-1]).
-measure('New Computers', 500, [2-5, 1-8], [0-1]).
-measure('Hire more staff', 900, [0-10, 1-20], [2-2, 3-3]).
-measure('Improve food quality', 200, [0-4, 1-2], []).
-measure('In house software', 1000, [0-10, 1-20], [0-3]).
+getCostImpact(_,_) :-
+  write('Invalid priority sum!'), fail.
 
-
-getCostImpact(AllCost,AllImpact):- findall(Priority,(criteria_priority(_,Priority)), AllPriorities),
-        findall(ImpactCost,(measure(_,Cost,Positive,Negative), getTotalImpact(Positive,Negative, 0, Impact,AllPriorities), ImpactCostBRound is Impact /Cost, ImpactCost is round(ImpactCostBRound)), AllImpact),
-        findall(Cost,measure(_,Cost,_,_), AllCost).
-        
-        
-getTotalImpact([] ,[], Helper, Impact,_):- Impact = Helper.
-                                  
-getTotalImpact([] ,[PN-IN|RN], Helper, Impact,AllPriorities):-  
-
-        nth0(PN, AllPriorities,NImp),
-        Newhelper is Helper -NImp*IN*1000,
-        getTotalImpact([],RN,Newhelper,Impact,AllPriorities).
-        
-getTotalImpact([PP-IP|RP], [], Helper, Impact,AllPriorities):-
-        nth0(PP, AllPriorities, Imp),     
-        Newhelper is Helper + (Imp *IP*1000),
-        getTotalImpact(RP,[],Newhelper,Impact,AllPriorities).           
-        
-getTotalImpact([PP-IP|RP],[PN-IN|RN], Helper, Impact,AllPriorities):-  
-        nth0(PP, AllPriorities, Imp),
-        nth0(PN, AllPriorities,NImp),
-        Newhelper is Helper + (Imp *IP*1000-NImp*IN*1000),
-        getTotalImpact(RP,RN,Newhelper,Impact,AllPriorities).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%THIS CODE IS WORKING%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-/*optimize(AllCost, AllImpact,ListResult, MaxBudget):-
-        constrain(AllCost, AllImpact,ListResult,[], MaxBudget),!.
-
-
-exceedsBudget(AllCost,Pos,_,_):-
-        length(AllCost,L),
-        Pos>L.
-
-exceedsBudget(AllCost,Pos,List,MaxBudget):-
-        nth1(Pos,AllCost,Elem),
-        (Elem>MaxBudget;member(Pos, List)),
-        NewPos is Pos+1,
-        exceedsBudget(AllCost,NewPos,List,MaxBudget).
-        
-        
-constrain(_, _,ListResult,List, 0):-ListResult=List.
-
-constrain(AllCost, _,ListResult,List, MaxBudget):-
-        exceedsBudget(AllCost,1,List,MaxBudget),
-        ListResult=List.
-
-constrain(AllCost, AllImpact,ListResult,List, MaxBudget):-
-          element(X,AllImpact,Y),
-          element(X,AllCost,Cost),
-          append(List, [X], NewList),
-          Cost #=< MaxBudget,
-          all_distinct(NewList),
-          maximize(labeling([],[Y,Cost]), Y), 
-          NewBudget is MaxBudget-Cost,
-          constrain(AllCost, AllImpact,ListResult,NewList, NewBudget).*/
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Measures the specific impact of one measure.
+measureImpact(AllPriorities, Cost, Positives, Negatives, Impact) :-
+  findall(Good, (member(PriorityNumber-Value, Positives), nth0(PriorityNumber, AllPriorities, Priority), Good is (Value*Priority*1000/Cost)), PositiveImpacts),
+  findall(Bad, (member(PriorityNumber-Value, Negatives), nth0(PriorityNumber, AllPriorities, Priority), Bad is (Value*Priority*1000/Cost)), NegativeImpacts),
+  sumlist(PositiveImpacts, SumPos), 
+  sumlist(NegativeImpacts, SumNeg),
+  ImpactRound is SumPos-SumNeg, 
+  Impact is round(ImpactRound).
 
 exceedsBudget(AllCost,Pos,_,_):-
         length(AllCost,L),
@@ -112,10 +68,6 @@ constrain(AllCost, AllImpact,ListResult,List,TotalEfeciency,Efeciency, MaxBudget
           constrain(AllCost, AllImpact,ListResult,NewList,TotalEfeciency,NewEfeciency,NewBudget).
 
 
-solveProblem(Budget):-
-               getCostImpact(AllCost,AllImpact),
-               optimize(AllCost,AllImpact,Result,Budget),
-               showResult(Result).
 
 showResult(Result):-
         findall(Name,(measure(Name,_,_,_)), AllMeasures),
---
+++ trabalho2/test_cases.pl
@@ -0,0 +1,22 @@
+% criteria_priority(Description, Priority).
+criteria_priority('Employee Efficiency', 0.1).
+criteria_priority('Public Relations', 0.15).
+criteria_priority('Software', 0.15).
+criteria_priority('Time', 0.2).
+criteria_priority('Structure of Company', 0.1).
+criteria_priority('Scalability', 0.05).
+criteria_priority('Hardware', 0.05).
+criteria_priority('Debugging', 0.12).
+criteria_priority('Hardware', 0.08).
+
+%measure(Description, Cost, Bonus([Id-Improvement]), Setbacks([Id-Impact])).
+measure('Tutorials', 200, [0-2, 1-4, 3-2, 5-1, 6-2], [2-1, 4-1,7-3,8-2]).
+measure('New Computers', 350, [0-1, 4-2, 5-6, 7-1, 8-2], [1-1, 2-1,3-3,6-2]).
+measure('Hire more staff', 250, [3-2, 4-1, 6-5, 7-5, 8-2], [0-3, 1-2,2-4,5-4]).
+measure('Improve food quality', 200, [0-1, 1-2, 2-6, 3-1, 4-2], [6-1, 7-1,8-3,5-3]).
+measure('In house software', 500, [1-2, 2-5, 4-2, 7-1, 8-4], [0-3, 3-2,5-3,6-2]).
+measure('Changes to team structure', 700, [1-2, 3-2, 5-6, 6-1, 8-2], [0-1, 2-1,4-3,7-2]).
+measure('Improvements to software development model', 800, [1-5, 3-3, 5-2, 7-4], [0-1, 2-1,4-3,7-2, 8-1]).
+measure('Better Software for Designers', 900, [3-5,5-3,6-2,7-4], [0-1,1-1,2-3,4-5,8-1]).
+measure('Better Software for Debugging', 400, [1-2,2-5,3-2,7-15], [0-1,4-1,5-3,6-5,8-4]).
+measure('Better Quality Control', 800, [1-2,5-5,6-2,7-15], [0-1,2-2,3-5,4-2,8-2).
